4617b3b43a65770946505c848a713e09ba3ac2db
myroslav1812 <myroslav1812@gmail.com>
2020-04-27 14:52:24 +0300

    add new path to index.js

---
+++ build/precache-manifest.6d34a899094b4f3eb658aac672451220.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0b0b64b22800fc727fc05e0d4dc721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "369615b0ef6122c86b1c",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "dbfd0e8bf3275d4bf127",
+    "url": "/static/css/main.041a3c66.chunk.css"
+  },
+  {
+    "revision": "369615b0ef6122c86b1c",
+    "url": "/static/js/2.1b29cba3.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.1b29cba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfd0e8bf3275d4bf127",
+    "url": "/static/js/main.64c07c4e.chunk.js"
+  },
+  {
+    "revision": "512b576e7e7d5ab7669a",
+    "url": "/static/js/runtime-main.8a2ee1dd.js"
+  },
+  {
+    "revision": "1de3789350dae1499b14b71184c26d93",
+    "url": "/static/media/bg.1de37893.jpg"
+  }
+]);
--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const { signUp, auth, refreshTokens, getProjects, putProject, deleteProject, getTasks, putTask, deleteTask, changeTaskStatus, updateProject, updateTask, checkToken, logOut, updateTaskPriority } = require('./controllers')
 const { checkAuth } = require('./middleware');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ruby-garage');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ruby-garage', { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('debug', true);
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(express.static(path
+    .join(__dirname, 'build')));
+
+
 
 app.post('/signup', signUp);
 app.post('/signin', auth);
@@ -29,6 +36,10 @@ app.post('/update-priority', checkAuth, updateTaskPriority);
 app.get('/projects', checkAuth, getProjects);
 app.get('/tasks/:id', checkAuth, getTasks);
 app.get('/check-token', checkAuth, checkToken);
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.delete('/projects/:id', checkAuth, deleteProject);
 app.delete('/task/:projectId/:id', checkAuth, deleteTask);
